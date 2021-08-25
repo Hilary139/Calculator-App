aec5a44fafc056fe56e6aeb199457e2bf27a1931
hilary139 <aghasilihilary@gmail.com>
2021-08-25 17:37:19 +0100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,303 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextAdd1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.AddBtn = New System.Windows.Forms.Button()
+        Me.AddAns = New System.Windows.Forms.TextBox()
+        Me.TextAdd2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ExitBtn = New System.Windows.Forms.Button()
+        Me.TextSub1 = New System.Windows.Forms.TextBox()
+        Me.TextSub2 = New System.Windows.Forms.TextBox()
+        Me.TextMul2 = New System.Windows.Forms.TextBox()
+        Me.TextDiv2 = New System.Windows.Forms.TextBox()
+        Me.TextDiv1 = New System.Windows.Forms.TextBox()
+        Me.TextMul1 = New System.Windows.Forms.TextBox()
+        Me.SubBtn = New System.Windows.Forms.Button()
+        Me.DivBtn = New System.Windows.Forms.Button()
+        Me.MultiBtn = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SubAns = New System.Windows.Forms.TextBox()
+        Me.MulAns = New System.Windows.Forms.TextBox()
+        Me.DivAns = New System.Windows.Forms.TextBox()
+        Me.Submit = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextAdd1
+        '
+        Me.TextAdd1.Location = New System.Drawing.Point(12, 90)
+        Me.TextAdd1.Name = "TextAdd1"
+        Me.TextAdd1.Size = New System.Drawing.Size(52, 23)
+        Me.TextAdd1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(129, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 15)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "CALCULATOR"
+        '
+        'AddBtn
+        '
+        Me.AddBtn.Location = New System.Drawing.Point(86, 89)
+        Me.AddBtn.Name = "AddBtn"
+        Me.AddBtn.Size = New System.Drawing.Size(46, 23)
+        Me.AddBtn.TabIndex = 2
+        Me.AddBtn.Text = "ADD"
+        Me.AddBtn.UseVisualStyleBackColor = True
+        '
+        'AddAns
+        '
+        Me.AddAns.Location = New System.Drawing.Point(265, 89)
+        Me.AddAns.Name = "AddAns"
+        Me.AddAns.Size = New System.Drawing.Size(98, 23)
+        Me.AddAns.TabIndex = 3
+        '
+        'TextAdd2
+        '
+        Me.TextAdd2.Location = New System.Drawing.Point(156, 90)
+        Me.TextAdd2.Name = "TextAdd2"
+        Me.TextAdd2.Size = New System.Drawing.Size(52, 23)
+        Me.TextAdd2.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(229, 89)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(30, 15)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "ANS"
+        '
+        'ExitBtn
+        '
+        Me.ExitBtn.Location = New System.Drawing.Point(133, 277)
+        Me.ExitBtn.Name = "ExitBtn"
+        Me.ExitBtn.Size = New System.Drawing.Size(75, 23)
+        Me.ExitBtn.TabIndex = 6
+        Me.ExitBtn.Text = "EXIT APP"
+        Me.ExitBtn.UseVisualStyleBackColor = True
+        '
+        'TextSub1
+        '
+        Me.TextSub1.Location = New System.Drawing.Point(12, 135)
+        Me.TextSub1.Name = "TextSub1"
+        Me.TextSub1.Size = New System.Drawing.Size(52, 23)
+        Me.TextSub1.TabIndex = 7
+        '
+        'TextSub2
+        '
+        Me.TextSub2.Location = New System.Drawing.Point(156, 136)
+        Me.TextSub2.Name = "TextSub2"
+        Me.TextSub2.Size = New System.Drawing.Size(52, 23)
+        Me.TextSub2.TabIndex = 8
+        '
+        'TextMul2
+        '
+        Me.TextMul2.Location = New System.Drawing.Point(156, 184)
+        Me.TextMul2.Name = "TextMul2"
+        Me.TextMul2.Size = New System.Drawing.Size(52, 23)
+        Me.TextMul2.TabIndex = 9
+        '
+        'TextDiv2
+        '
+        Me.TextDiv2.Location = New System.Drawing.Point(156, 231)
+        Me.TextDiv2.Name = "TextDiv2"
+        Me.TextDiv2.Size = New System.Drawing.Size(52, 23)
+        Me.TextDiv2.TabIndex = 10
+        '
+        'TextDiv1
+        '
+        Me.TextDiv1.Location = New System.Drawing.Point(12, 232)
+        Me.TextDiv1.Name = "TextDiv1"
+        Me.TextDiv1.Size = New System.Drawing.Size(52, 23)
+        Me.TextDiv1.TabIndex = 11
+        '
+        'TextMul1
+        '
+        Me.TextMul1.Location = New System.Drawing.Point(12, 185)
+        Me.TextMul1.Name = "TextMul1"
+        Me.TextMul1.Size = New System.Drawing.Size(52, 23)
+        Me.TextMul1.TabIndex = 12
+        '
+        'SubBtn
+        '
+        Me.SubBtn.Location = New System.Drawing.Point(86, 135)
+        Me.SubBtn.Name = "SubBtn"
+        Me.SubBtn.Size = New System.Drawing.Size(46, 23)
+        Me.SubBtn.TabIndex = 13
+        Me.SubBtn.Text = "SUB"
+        Me.SubBtn.UseVisualStyleBackColor = True
+        '
+        'DivBtn
+        '
+        Me.DivBtn.Location = New System.Drawing.Point(86, 231)
+        Me.DivBtn.Name = "DivBtn"
+        Me.DivBtn.Size = New System.Drawing.Size(46, 23)
+        Me.DivBtn.TabIndex = 14
+        Me.DivBtn.Text = "DIV"
+        Me.DivBtn.UseVisualStyleBackColor = True
+        '
+        'MultiBtn
+        '
+        Me.MultiBtn.Location = New System.Drawing.Point(86, 184)
+        Me.MultiBtn.Name = "MultiBtn"
+        Me.MultiBtn.Size = New System.Drawing.Size(46, 23)
+        Me.MultiBtn.TabIndex = 15
+        Me.MultiBtn.Text = "MULTI"
+        Me.MultiBtn.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(229, 139)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(30, 15)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "ANS"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(229, 192)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(30, 15)
+        Me.Label4.TabIndex = 17
+        Me.Label4.Text = "ANS"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(229, 235)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(30, 15)
+        Me.Label5.TabIndex = 18
+        Me.Label5.Text = "ANS"
+        '
+        'SubAns
+        '
+        Me.SubAns.Location = New System.Drawing.Point(265, 136)
+        Me.SubAns.Name = "SubAns"
+        Me.SubAns.Size = New System.Drawing.Size(98, 23)
+        Me.SubAns.TabIndex = 19
+        '
+        'MulAns
+        '
+        Me.MulAns.Location = New System.Drawing.Point(265, 184)
+        Me.MulAns.Name = "MulAns"
+        Me.MulAns.Size = New System.Drawing.Size(98, 23)
+        Me.MulAns.TabIndex = 20
+        '
+        'DivAns
+        '
+        Me.DivAns.Location = New System.Drawing.Point(265, 231)
+        Me.DivAns.Name = "DivAns"
+        Me.DivAns.Size = New System.Drawing.Size(98, 23)
+        Me.DivAns.TabIndex = 21
+        '
+        'Submit
+        '
+        Me.Submit.Location = New System.Drawing.Point(295, 37)
+        Me.Submit.Name = "Submit"
+        Me.Submit.Size = New System.Drawing.Size(68, 23)
+        Me.Submit.TabIndex = 24
+        Me.Submit.Text = "NOTICE"
+        Me.Submit.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(197, 41)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(79, 15)
+        Me.Label6.TabIndex = 25
+        Me.Label6.Text = "Easy To Use ..."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(375, 312)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Submit)
+        Me.Controls.Add(Me.DivAns)
+        Me.Controls.Add(Me.MulAns)
+        Me.Controls.Add(Me.SubAns)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.MultiBtn)
+        Me.Controls.Add(Me.DivBtn)
+        Me.Controls.Add(Me.SubBtn)
+        Me.Controls.Add(Me.TextMul1)
+        Me.Controls.Add(Me.TextDiv1)
+        Me.Controls.Add(Me.TextDiv2)
+        Me.Controls.Add(Me.TextMul2)
+        Me.Controls.Add(Me.TextSub2)
+        Me.Controls.Add(Me.TextSub1)
+        Me.Controls.Add(Me.ExitBtn)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextAdd2)
+        Me.Controls.Add(Me.AddAns)
+        Me.Controls.Add(Me.AddBtn)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextAdd1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextAdd1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents AddBtn As Button
+    Friend WithEvents AddAns As TextBox
+    Friend WithEvents TextAdd2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ExitBtn As Button
+    Friend WithEvents TextSub1 As TextBox
+    Friend WithEvents TextSub2 As TextBox
+    Friend WithEvents TextMul2 As TextBox
+    Friend WithEvents TextDiv2 As TextBox
+    Friend WithEvents TextDiv1 As TextBox
+    Friend WithEvents TextMul1 As TextBox
+    Friend WithEvents SubBtn As Button
+    Friend WithEvents DivBtn As Button
+    Friend WithEvents MultiBtn As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents SubAns As TextBox
+    Friend WithEvents MulAns As TextBox
+    Friend WithEvents DivAns As TextBox
+    Friend WithEvents Submit As Button
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,67 @@
+﻿Public Class Form1      ' this starts the application, it holds other block of code used in the application 
+
+    ' this is the Addition Code 
+
+    Private Sub AddBtn_Click(sender As Object, e As EventArgs) Handles AddBtn.Click
+        Dim AddOne As Integer = CInt(TextAdd1.Text)
+        Dim AddTwo As Integer = CInt(TextAdd2.Text)
+        Dim AnswerAdd As Integer = CInt(AddOne + AddTwo)
+
+        AddAns.Text = CStr(AnswerAdd)
+
+    End Sub
+
+
+
+    ' this is the Subtraction Code 
+
+    Private Sub SubBtn_Click(sender As Object, e As EventArgs) Handles SubBtn.Click
+        Dim SubOne As Integer = CInt(TextSub1.Text)
+        Dim SubTwo As Integer = CInt(TextSub2.Text)
+        Dim AnswerSub As Integer = CInt(SubOne - SubTwo)
+
+        SubAns.Text = CStr(AnswerSub)
+
+    End Sub
+
+
+    ' this is the Multiplication Code 
+
+    Private Sub MultiBtn_Click(sender As Object, e As EventArgs) Handles MultiBtn.Click
+        Dim MultiOne As Integer = CInt(TextMul1.Text)
+        Dim MultiTwo As Integer = CInt(TextMul2.Text)
+        Dim AnswerMulti As Integer = CInt(MultiOne * MultiTwo)
+
+        MulAns.Text = CStr(AnswerMulti)
+
+    End Sub
+
+
+    ' this is the Division Code
+
+    Private Sub DivBtn_Click(sender As Object, e As EventArgs) Handles DivBtn.Click
+        Dim DivOne As Integer = CInt(TextDiv1.Text)
+        Dim DivTwo As Integer = CInt(TextDiv2.Text)
+        Dim AnswerDiv As Integer = CInt(DivOne / DivTwo)
+
+        DivAns.Text = CStr(AnswerDiv)
+    End Sub
+
+
+
+    ' this is the Exit Button Code 
+    Private Sub ExitBtn_Click(sender As Object, e As EventArgs) Handles ExitBtn.Click
+        Me.Close()  ' used to exit the application 
+
+    End Sub
+
+
+
+    Private Sub Submit_Click(sender As Object, e As EventArgs) Handles Submit.Click
+        MessageBox.Show("Thank You!! You Can Now Use The Application :) ")
+    End Sub
+
+
+End Class
+
+' the End statement is used to end every block of code 
